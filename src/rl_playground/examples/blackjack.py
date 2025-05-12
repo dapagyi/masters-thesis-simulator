f7c74b6024df1ff8ac9c85644afb82bb2fed9833
David Apagyi <apagyi.david@gmail.com>
2025-05-12 19:46:41 +0300

    feat(example): add blackjack implementation for RL training

---
+++ src/rl_playground/examples/blackjack.py
@@ -0,0 +1,140 @@
+from collections import defaultdict
+from pathlib import Path
+
+import gymnasium as gym
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import trange
+
+
+class BlackjackAgent:
+    def __init__(
+        self,
+        env: gym.Env,
+        learning_rate: float,
+        initial_epsilon: float,
+        epsilon_decay: float,
+        final_epsilon: float,
+        discount_factor: float = 0.95,
+    ):
+        """Initialize a Reinforcement Learning agent with an empty dictionary
+        of state-action values (q_values), a learning rate and an epsilon.
+
+        Args:
+            env: The training environment
+            learning_rate: The learning rate
+            initial_epsilon: The initial epsilon value
+            epsilon_decay: The decay for epsilon
+            final_epsilon: The final epsilon value
+            discount_factor: The discount factor for computing the Q-value
+
+        """
+        self.env = env
+        self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
+
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        self.training_error = []
+
+    def get_action(self, obs: tuple[int, int, bool]) -> int:
+        """Returns the best action with probability (1 - epsilon)
+        otherwise a random action with probability epsilon to ensure exploration.
+        """
+        # with probability epsilon return a random action to explore the environment
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        # with probability (1 - epsilon) act greedily (exploit)
+        return int(np.argmax(self.q_values[obs]))
+
+    def update(
+        self,
+        obs: tuple[int, int, bool],
+        action: int,
+        reward: float,
+        terminated: bool,
+        next_obs: tuple[int, int, bool],
+    ):
+        """Updates the Q-value of an action."""
+        future_q_value = (not terminated) * np.max(self.q_values[next_obs])
+        temporal_difference = reward + self.discount_factor * future_q_value - self.q_values[obs][action]
+
+        self.q_values[obs][action] = self.q_values[obs][action] + self.lr * temporal_difference
+        self.training_error.append(temporal_difference)
+
+    def decay_epsilon(self):
+        self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+
+def get_moving_avgs(arr, window, convolution_mode):
+    return np.convolve(np.array(arr).flatten(), np.ones(window), mode=convolution_mode) / window
+
+
+def train_blackjack_agent() -> None:
+    learning_rate: float = 0.01
+    n_episodes = 100_000
+    start_epsilon: float = 1.0
+    epsilon_decay: float = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+    final_epsilon: float = 0.1
+
+    env = gym.make("Blackjack-v1", sab=False)
+    env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
+
+    agent = BlackjackAgent(
+        env=env,
+        learning_rate=learning_rate,
+        initial_epsilon=start_epsilon,
+        epsilon_decay=epsilon_decay,
+        final_epsilon=final_epsilon,
+    )
+    for _episode in trange(n_episodes):
+        obs, info = env.reset()
+        done = False
+
+        # play one episode
+        while not done:
+            action = agent.get_action(obs)
+            next_obs, reward, terminated, truncated, info = env.step(action)
+
+            # update the agent
+            agent.update(obs, action, reward, terminated, next_obs)
+
+            # update if the environment is done and the current obs
+            done = terminated or truncated
+            obs = next_obs
+
+        agent.decay_epsilon()
+
+    env.close()
+
+    # Smooth over a 500 episode window
+    rolling_length = 500
+    fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
+
+    axs[0].set_title("Episode rewards")
+    reward_moving_average = get_moving_avgs(env.return_queue, rolling_length, "valid")
+    axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
+
+    axs[1].set_title("Episode lengths")
+    length_moving_average = get_moving_avgs(env.length_queue, rolling_length, "valid")
+    axs[1].plot(range(len(length_moving_average)), length_moving_average)
+
+    axs[2].set_title("Training Error")
+    training_error_moving_average = get_moving_avgs(agent.training_error, rolling_length, "same")
+    axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
+    plt.tight_layout()
+
+    # Save the plot
+    results_dir = Path(".") / "results"
+    results_dir.mkdir(parents=True, exist_ok=True)
+    plt.savefig(results_dir / f"blackjack_{n_episodes}.png")
+    plt.close(fig)
+    print("Plot saved to", results_dir / f"blackjack_{n_episodes}.png")
+
+
+if __name__ == "__main__":
+    train_blackjack_agent()
