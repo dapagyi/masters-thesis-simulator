eb07b1dc89b28a292fe1c31e00a0a881f2208a60
David Apagyi <apagyi.david@gmail.com>
2025-05-17 00:30:28 +0200

    feat(example): train an agent using SB3 and record its performance in a video

---
+++ src/rl_playground/examples/record_agent.py
@@ -0,0 +1,43 @@
+"""Train an agent using Stable Baselines3 and record its performance in a video.
+
+https://gymnasium.farama.org/main/introduction/record_agent/
+https://stable-baselines3.readthedocs.io/en/master/guide/quickstart.html
+"""
+
+import gymnasium as gym
+from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
+from stable_baselines3 import A2C
+
+
+def main():
+    # Environment for training
+    train_env = gym.make("CartPole-v1", render_mode="rgb_array")
+
+    # Train the model
+    model = A2C("MlpPolicy", train_env, verbose=1)
+    model.learn(total_timesteps=10_000)  # Train the model
+    train_env.close()  # Close the training environment
+
+    # Environment for evaluation and video recording
+    eval_env = gym.make("CartPole-v1", render_mode="rgb_array")
+    eval_env = RecordVideo(
+        eval_env, video_folder="results/cartpole-agent", name_prefix="eval", episode_trigger=lambda x: True
+    )
+    eval_env = RecordEpisodeStatistics(eval_env, buffer_length=1)
+    obs, info = eval_env.reset()
+    episode_over = False
+    while not episode_over:
+        action, _state = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = eval_env.step(action)
+
+        episode_over = terminated or truncated
+
+    eval_env.close()
+
+    print(f"Episode time taken: {eval_env.time_queue}")
+    print(f"Episode total rewards: {eval_env.return_queue}")
+    print(f"Episode lengths: {eval_env.length_queue}")
+
+
+if __name__ == "__main__":
+    main()
